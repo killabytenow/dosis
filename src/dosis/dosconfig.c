93afb7113e45eeecdbf575cc4fabe61ee22d808c
Gerardo García Peña <gerardo@togusa.kung-foo.dhs.org>
2008-12-22 01:08:55 +0100

    new: moar moar moar

---
+++ src/dosis/dosconfig.c
@@ -0,0 +1,649 @@
+/*****************************************************************************
+ * config.c
+ *
+ * Configuration structure.
+ *
+ * ---------------------------------------------------------------------------
+ * dosis - DoS: Internet Sodomizer
+ *   (C) 2008 Gerardo García Peña <gerardo@kung-foo.dhs.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the Free
+ *   Software Foundation; either version 2 of the License, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *   more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc., 51
+ *   Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *****************************************************************************/
+
+#include <config.h>
+#include <datadir.h>
+#include "dosconfig.h"
+#include "log.h"
+
+/* default config and global configuration pointer */
+static DOS_CONFIG cfg_default = {
+  /* verbosity                                 */ LOG_LEVEL_LOG,
+  /* source address in packet tests            */ { 0 },
+  /* target address in packet tests            */ { 0 },
+  /* if set source port is randomly choosed    */ 1,
+  /* if set target port is randomly choosed    */ 1,
+  /* total number of threads                   */ 2,
+  /* threads used to read from ip_queue        */ 1,
+  /* packets sent in each packet burst         */ 1,
+  /* hits per second                           */ 1.0,
+  /* cwait                                     */ 3000000,
+  /* rwait                                     */ 10000000,
+  /* time to keep simulating real users        */ 5,
+  /* req                                       */ NULL,
+  /* request size                              */ 0,
+
+  /* configuration data set by application based on user options */
+};
+DOS_CONFIG *cfg = &cfg_default;
+
+/* private object's global variables */
+static struct option *long_options = NULL;
+static char *short_options = NULL;
+static int help_flag;
+
+/* prototypes for object's internal functions */
+static int dos_vhandler_addr(DOS_PARAMETER *, char *, int);
+static int dos_vhandler_bool(DOS_PARAMETER *, char *, int);
+static int dos_vhandler_int(DOS_PARAMETER *, char *, int);
+static int dos_vhandler_string(DOS_PARAMETER *, char *, int);
+static int dos_vhandler_nslist(DOS_PARAMETER *, char *, int);
+static int dos_vhandler_output_format(DOS_PARAMETER *, char *, int);
+
+#define CFGOFF(f)         offsetof(DOS_CONFIG,f)
+DOS_PARAMETER dos_param_list[] = {
+  { "clients",        "no", CFGOFF(c),         dos_vhandler_int           },
+  { "conn-timeout",   NULL, CFGOFF(cwait),     dos_vhandler_int           },
+  { "target",         NULL, CFGOFF(dhost),     dos_vhandler_addr          },
+  { "target-port",    NULL, CFGOFF(dhost),     dos_vhandler_port          },
+  { "hit-ratio",      NULL, CFGOFF(hits),      dos_vhandler_int           },
+  { "listen",         NULL, CFGOFF(l),         dos_vhandler_int           },
+  { "npackets",       NULL, CFGOFF(packets),   dos_vhandler_int           },
+  { "request",        NULL, CFGOFF(request),   dos_vhandler_string        },
+  { "reply-timeout",  NULL, CFGOFF(rwait),     dos_vhandler_int           },
+  { "source",         NULL, CFGOFF(shost),     dos_vhandler_addr          },
+  { "source-port",    NULL, CFGOFF(shost),     dos_vhandler_port          },
+  { "run-time",       NULL, CFGOFF(runtime),   dos_vhandler_int           },
+  { "verbosity",       "2", CFGOFF(verbosity), dos_vhandler_verbosity     },
+  { NULL,                0, 0,                 NULL                       }
+};
+
+static int dos_help_opt_trigger(char *optarg)
+{
+  help_flag = -1;
+  return 0;
+}
+
+#define CMD_OPTIONS_N (sizeof(cmd_options) / sizeof(DOS_CMD_OPTION))
+DOS_CMD_OPTION cmd_options[] = {
+  { 'c', "clients",       0, "clients",       "yes", NULL                  },
+  { 'C', "conn-timeout",  0, "conn-timeout",  "yes", NULL                  },
+  { 'd', "target",        0, "target",        "yes", NULL                  },
+  { 'D', "target-port",   1, "target-port",    NULL, NULL                  },
+  { 'h', "help",          0, NULL,             NULL, dos_help_opt_trigger  },
+  { 'H', "hit-ratio",     0, "hit-ratio",       "4", NULL                  },
+  { 'l', "listen",        0, "listen",        "yes", NULL                  },
+  { 'p', "npackets",      1, "npackets",       NULL, NULL                  },
+  { 'q', "quiet",         0, "verbosity",       "0", NULL                  },
+  { 'r', "request",       1, "request",        NULL, NULL                  },
+  { 'R', "reply-timeout", 1, "reply-timeout",  NULL, NULL                  },
+  { 's', "source",        1, "source",         NULL, NULL                  },
+  { 'S', "source-port",   1, "source-port",    NULL, NULL                  },
+  { 'T', "run-time",      0, "run-time",      "yes", NULL                  },
+  { 'v', "verbosity",     2, "verbosity",       "3", NULL                  },
+  {   0, NULL,            0, NULL,             NULL, NULL                  },
+};
+
+/*****************************************************************************
+ * Module Private Functions
+ *****************************************************************************/
+
+static int dos_vhandler_bool(DOS_PARAMETER *c, char *buff, int rbuffsize)
+{
+  char *ttable[] = { "yes", "true",  "y", "enable",  "enabled",  "1", NULL };
+  char *ftable[] = { "no",  "false", "n", "disable", "disabled", "0", NULL };
+  char **values[] = { ftable, ttable };
+  int i;
+  char **v;
+  int *target = ((void *) cfg) + c->cfg_offset;
+  int ret;
+
+  if(!buff)
+    rbuffsize = 0;
+
+  if(rbuffsize >= 0)
+    ret = snprintf(buff, rbuffsize, "%s", *target ? "yes" : "no");
+  else
+    if(buff)
+    {
+      for(i = 0; i < 2; i++)
+        for(v = values[i]; *v; v++)
+          if(!strcasecmp(buff, *v))
+          {
+            *target = i * -1;
+            ret = 0;
+          }
+
+      if(ret < 0)
+        D_ERR("%s: Bad syntax, I don't know how to interpret '%s'.", c->name, buff);
+    }
+
+  return ret;
+}
+
+static int dos_vhandler_int(DOS_PARAMETER *c, char *buff, int rbuffsize)
+{
+  int *target = ((void *) cfg) + c->cfg_offset;
+  int ret = 0;
+
+  if(!buff)
+    rbuffsize = 0;
+
+  if(rbuffsize >= 0)
+    ret = snprintf(buff, rbuffsize, "%d", *target);
+  else
+    if(buff)
+      *target = atoi(buff);
+
+  return ret;
+}
+
+static int dos_vhandler_string(DOS_PARAMETER *c, char *buff, int rbuffsize)
+{
+  char **target = ((void *) cfg) + c->cfg_offset;
+  int ret = 0;
+
+  if(!buff)
+    rbuffsize = 0;
+
+  if(rbuffsize >= 0)
+    ret = snprintf(buff, rbuffsize, "%s", *target);
+  else {
+    if(*target != NULL)
+      free(*target);
+    D_ERR("Going to free %08x", *target);
+    D_ERR("Going to set %08x", buff);
+    D_ERR("Going to set %s", buff);
+    if(!buff)
+      *target = NULL;
+    else
+      if((*target = strdup(buff)) == NULL)
+        D_FAT("Cannot alloc memory for string '%s'.", buff);
+    D_DBG("Set to %08x", *target);
+  }
+
+  return 0;
+}
+
+static int dos_vhandler_nslist(DOS_PARAMETER *c, char *buff, int rbuffsize)
+{
+  int r;
+
+  r = dos_vhandler_string(c, buff, rbuffsize);
+
+  if(r < 0 && buff)
+    nslist_load();
+
+  return r;
+}
+
+static int dos_vhandler_output_format(DOS_PARAMETER *c, char *buff, int rbuffsize)
+{
+  char *formats[] = { "txt", "csv" };
+  int *target = ((void *) cfg) + c->cfg_offset;
+  int i, ret;
+
+  if(!buff)
+    rbuffsize = 0;
+
+  if(rbuffsize >= 0)
+    ret = snprintf(buff, rbuffsize, "%s", formats[*target]);
+  else
+    if(buff)
+    {
+      for(i = 0; i < sizeof(formats) / sizeof(char *); i++)
+        if(!strcasecmp(buff, formats[i]))
+        {
+          *target = i;
+          ret = 0;
+        }
+
+      if(ret < 0)
+        D_ERR("%s: Bad output mode '%s'.", c->name, buff);
+    }
+
+  return ret;
+}
+
+/*****************************************************************************
+ * Command Line Parser
+ *
+ *   Following code parses command line input (with getopt(3) help), updating
+ *   current configuration (cfg).
+ *
+ *****************************************************************************/
+
+void dos_param_set(char *param, char *value)
+{
+  DOS_PARAMETER *p;
+
+  for(p = dos_param_list; p->name && strcmp(p->name, param); p++)
+    ;
+  if(!p->name)
+  {
+   D_ERR("Parameter configuration '%s' does not exists.", param);
+   return;
+  }
+  D_ERR("<<<<<<<<<<<<%s>>>>>>>>>>>>", p->name);
+
+  if(p->par_handler(p, value, -1))
+    D_ERR("Cannot configure parameter '%s'.", param);
+}
+
+static DOS_CMD_OPTION *find_cmd_option(int c)
+{
+  int i;
+
+  if(c >= 0 && c < CMD_OPTIONS_N)
+    return cmd_options + c;
+
+  for(i = 0; i < CMD_OPTIONS_N; i++)
+    if(c == cmd_options[i].shortopt)
+      return cmd_options + i;
+
+  return NULL;
+}
+
+static DOS_COMMAND *find_cmd(char *s)
+{
+  DOS_COMMAND *r;
+  int i, j;
+
+  r = NULL;
+  for(i = 0; !r && dos_cmd_list[i]; i++)
+    for(j = 0; !r && dos_cmd_list[i]->name[j]; j++)
+      if(!strcasecmp(dos_cmd_list[i]->name[j], s))
+        r = dos_cmd_list[i];
+
+  return r;
+}
+
+static DOS_COMMAND *dos_config_parse_command(int argc, char **argv, int *error)
+{
+  int c, fin, i,
+      option_index = 0;
+  char *s;
+  DOS_CMD_OPTION *opt;
+  DOS_COMMAND *cmd;
+  int cmd_index, par_index;
+
+  /* configure and reset getopt */
+  *error = 0;
+  opterr = 0;
+  optind = 1;
+
+  /* get options */
+  fin = 0;
+  while(!fin)
+  {
+    c = getopt_long(argc, argv, short_options, long_options, &option_index);
+
+    if(c > 0)
+    {
+      if((opt = find_cmd_option(c)) == NULL)
+      {
+        D_ERR("Invalid option '%s'.", argv[optind-1]);
+        goto error;
+      }
+      if(!optarg)
+        s = NULL;
+      else
+        if((s = strdup(optarg)) == NULL)
+          D_FAT("Cannot duplicate string memory.");
+      if(!opt->handler || !opt->handler(optarg))
+        if(opt->param && (s || opt->set_val))
+          dos_param_set(opt->param, s ? s : opt->set_val);
+      if(s)
+        free(s);
+    } else
+      fin = -1;
+  }
+
+  /* get command */
+  cmd_index = optind;
+  par_index = optind +1;
+
+  /* search command */
+  cmd = cmd_index < argc
+          ? find_cmd(argv[cmd_index])
+          : NULL;
+
+  /* if help flag enabled then use 'help' command */
+  if(help_flag)
+  {
+    help_flag = 0;
+    if(cmd)
+    {
+      par_index = cmd_index;
+      cfg->nparams = 1;
+    } else
+      cfg->nparams = 0;
+    cmd = find_cmd("help");
+  } else
+    cfg->nparams = argc - par_index;
+
+  if(!cmd && cmd_index < argc)
+  {
+    if(cfg->verbosity < LOG_LEVEL_DEBUG)
+      D_ERR("Command not found.");
+    else
+      D_ERR("Bad bad boy ... you are inventing the stupid command '%s'. DIE DIE DIE MTF!!!!", argv[cmd_index]);
+    goto error;
+  }
+
+  /* check num of parameters and copy them if they are ok */
+  if(cmd)
+  {
+    if(cmd->min_pars > cfg->nparams)
+    {
+      D_ERR("Command needs at least %d parameters.", cmd->min_pars);
+      goto error;
+    }
+
+    if(cmd->max_pars > 0 && cmd->max_pars < cfg->nparams)
+    {
+      D_ERR("Too much parameters for command.");
+      goto error;
+    }
+
+    if(cfg->nparams == 0)
+      cfg->params = NULL;
+    else
+      if((cfg->params = calloc(cfg->nparams, sizeof(char *))) == NULL)
+        D_FAT("No memory for parameters.");
+
+    for(i = 0; i < cfg->nparams; i++)
+      if((cfg->params[i] = strdup(argv[i + par_index])) == NULL)
+        D_FAT("Cannot copy parameter %d (%s).", i + par_index, argv[i + par_index]);
+  }
+
+  if(cfg->verbosity < -1 || cfg->verbosity > LOG_LEVEL_ALL)
+  {
+    D_ERR("Verbosity out of bounds.");
+    goto error;
+  }
+  if(cfg->mfails < 0)
+  {
+    D_FAT("--max-fail cannot be negative."); 
+    goto error;
+  }
+
+  return cmd;
+
+error:
+  *error = 1;
+  return NULL;
+}
+
+/*****************************************************************************
+ * Manage configuration stack
+ *****************************************************************************/
+
+void dos_config_push(void)
+{
+  DOS_CONFIG *old;
+
+  old = cfg;
+  if((cfg = malloc(sizeof(DOS_CONFIG))) == NULL)
+    D_FAT("Cannot alloc memory for configuration.");
+
+  memcpy(cfg, old, sizeof(DOS_CONFIG));
+
+  cfg->previous = old;
+}
+
+void dos_config_pop(void)
+{
+  DOS_CONFIG *old;
+  int i;
+
+  if(!cfg->previous)
+  {
+    D_WRN("Top configuration is active. No pop possible.");
+    return;
+  }
+
+  /* pop config */
+  old = cfg;
+  cfg = cfg->previous;
+
+  /* free mem, close handles, etc... */
+  if(old->logfile && cfg->logfile != old->logfile)
+    fclose(old->logfile);
+  if(old->params && cfg->params != old->params)
+  {
+    for(i = 0; i < old->nparams; i++)
+      if(old->params[i])
+        free(old->params[i]);
+    free(old->params);
+  }
+  if(old->dicfile && cfg->dicfile != old->dicfile)
+    free(old->dicfile);
+  if(old->nslfile && cfg->nslfile != old->nslfile)
+    free(old->nslfile);
+  if(old->dumpfile && cfg->dumpfile != old->dumpfile)
+    free(old->dumpfile);
+  if(old->nameservers && cfg->nameservers != old->nameservers)
+    nslist_free(old->nameservers);
+}
+
+void dos_config_pop_and_merge(void)
+{
+  DOS_CONFIG *old;
+  int i;
+
+  if(!cfg->previous)
+  {
+    D_WRN("Top configuration is active. No pop possible.");
+    return;
+  }
+
+  /* pop config */
+  old = cfg;
+  cfg = cfg->previous;
+
+  /* free mem, close handles, etc... */
+
+  /* params are not merged */
+  if(old->params && cfg->params != old->params)
+  {
+    for(i = 0; i < old->nparams; i++)
+      if(old->params[i])
+        free(old->params[i]);
+    free(old->params);
+  }
+}
+
+/*****************************************************************************
+ * Initialization and finalization routines
+ *****************************************************************************/
+
+static void dos_config_fini(void)
+{
+  int i;
+
+  if(cfg)
+  {
+    if(cfg->logfile)
+      fclose(cfg->logfile);
+
+    if(cfg->params)
+    {
+      for(i = 0; i < cfg->nparams; i++)
+        if(cfg->params[i])
+          free(cfg->params[i]);
+      free(cfg->params);
+    }
+    if(cfg->dicfile)
+      free(cfg->dicfile);
+    if(cfg->nslfile)
+      free(cfg->nslfile);
+    if(cfg->dumpfile)
+      free(cfg->dumpfile);
+    if(cfg->nameservers)
+      nslist_free(cfg->nameservers);
+    free(cfg);
+    cfg = NULL;
+  }
+
+  if(short_options)
+    free(short_options);
+  if(long_options)
+    free(long_options);
+}
+
+static char *set_default_file(char **paths, char *file)
+{
+  struct stat buf;
+  char tmp[PATH_MAX], *r, *t;
+
+  /* in the worst case (file not found) we will return */
+  /* directly a copy of string pointed by 'file'       */
+  r = file;
+
+  /* search file 'file' in the list of 'paths' */
+  for(; *paths; paths++)
+  {
+    if(strlen(*paths) > 0)
+    {
+      if(snprintf(tmp, PATH_MAX, "%s%s%s",
+                  *paths, (*paths)[strlen(*paths)-1] != '/' ? "/" : "", file) > PATH_MAX)
+        D_FAT("String '%s/%s' is longer than PATH_MAX characters (%d).",
+              *paths, file, PATH_MAX);
+      t = tmp;
+    } else
+      t = file;
+
+    if(!stat(t, &buf) && S_ISREG(buf.st_mode))
+    {
+      r = t;
+      break;
+    }
+  }
+
+  /* return a copy */
+  if((r = strdup(r)) == NULL)
+    D_FAT("No memory for path '%s'.", tmp);
+
+  return r;
+}
+
+DOS_COMMAND *dos_config_init(int argc, char **argv, int *error)
+{
+  DOS_COMMAND *cmd;
+  int i, j;
+  char *s;
+  char *datadirs[] = {
+    ".",
+    DOS_PATH_DATA,
+    "/usr/share",
+    NULL
+  };
+  char *etcdirs[] = {
+    ".",
+    DOS_PATH_ETC,
+    "/etc",
+    NULL
+  };
+
+  /* first of all get concious about dead */
+  if(atexit(dos_config_fini))
+    D_FAT("Cannot set finalization routine.");
+
+  /* initialize getopt tables */
+  if(!(short_options = calloc((CMD_OPTIONS_N * 2) + 1, sizeof(char)))
+  || !(long_options = calloc(CMD_OPTIONS_N + 1, sizeof(struct option))))
+    D_FAT("No memory for getopt tables.");
+
+  s = short_options;
+  j = 0;
+  for(i = 0; i < CMD_OPTIONS_N; i++)
+  {
+    if(cmd_options[i].shortopt)
+    {
+      *s++ = cmd_options[i].shortopt;
+      if(cmd_options[i].has_arg == 1)
+        *s++ = ':';
+    }
+    if(cmd_options[i].longopt)
+    {
+      long_options[j].name    = cmd_options[i].longopt;
+      long_options[j].has_arg = cmd_options[i].has_arg;
+      long_options[j].flag    = NULL;
+      long_options[j].val     = cmd_options[i].shortopt
+                                  ? cmd_options[i].shortopt
+                                  : j;
+      j++;
+    }
+  }
+  *s = '\0';
+
+  /* create the base configuration */
+  if((cfg = malloc(sizeof(DOS_CONFIG))) == NULL)
+    D_FAT("Cannot alloc space for global configuration.");
+
+  /* set basic configuration */
+  memcpy(cfg, &cfg_default, sizeof(DOS_CONFIG));
+  dos_param_set("names",   set_default_file(datadirs, "dionisio.dic"));
+  dos_param_set("ns-list", set_default_file(etcdirs,  "nslist.txt"));
+
+  /* read config and command from command line */
+  cmd = dos_config_parse_command(argc, argv, error);
+
+  if(*error)
+    return cmd;
+
+  /* print program header and config (if debug verbosity enabled) */
+  dos_help_program_header();
+
+  D_DBG("Configuration");
+  D_DBG("  verbosity level             = %d", cfg->verbosity);
+  D_DBG("  follow new domains mode     = %d", cfg->follow);
+  D_DBG("  nameserver max-fails        = %d", cfg->mfails);
+  D_DBG("  name server list file       = %s", cfg->nslfile);
+  D_DBG("  dictionary file             = %s", cfg->dicfile);
+  D_DBG("  add comments                = %s", cfg->comments ? "yes" : "no");
+  D_DBG("  output module               = %d", cfg->output);
+  D_DBG("  do host analysis            = %s", cfg->test.host ? "yes" : "no");
+  D_DBG("  do reverse analysis         = %s", cfg->test.rev  ? "yes" : "no");
+  D_DBG("  do zone transfer analysis   = %s", cfg->test.axfr ? "yes" : "no");
+  D_DBG("  do domain analysis          = %s", cfg->test.dom  ? "yes" : "no");
+  D_DBG("  memorize non-existent names = %s", cfg->memorize  ? "yes" : "no");
+  if(cmd)
+  {
+    D_DBG("  command                     = %s", cmd->name[0]);
+    if(cfg->params)
+    {
+      D_DBG("  printing parameters (%d)", cfg->nparams);
+      for(i = 0; i < cfg->nparams; i++)
+        D_DBG("    %02d [%s]", i+1, cfg->params[i]);
+    } else
+      D_DBG("  no parameters pased.");
+  } else
+    D_DBG("  no command; launching a command line interface (CLI).");
+
+  return cmd;
+}
+
---
+++ src/dosis/dosconfig.h
@@ -0,0 +1,119 @@
+/*****************************************************************************
+ * dioconfig.h
+ *
+ * DioNiSio configuration structure.
+ *
+ * ---------------------------------------------------------------------------
+ * DioNiSio - DNS scanner
+ *   (C) 2006-2008 Gerardo García Peña
+ *
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the Free
+ *   Software Foundation; either version 2 of the License, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but WITHOUT
+ *   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *   more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc., 51
+ *   Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *****************************************************************************/
+
+#ifndef __DOSCONFIG_H__
+#define __DOSCONFIG_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <config.h>
+#include "ip.h"
+
+typedef struct __TAG_config {
+  int             verbosity;
+  INET_ADDR       shost;        /* source address in packet tests            */
+  INET_ADDR       dhost;        /* target address in packet tests            */
+  u_int8_t        rsport;       /* if set source port is randomly choosed    */
+  u_int8_t        rdport;       /* if set target port is randomly choosed    */
+  u_int32_t       c;            /* total number of threads                   */
+  u_int32_t       l;            /* threads used to read from ip_queue        */
+  u_int64_t       packets;      /* packets sent in each packet burst         */
+  double          hits;         /* packets per second                        */
+  u_int32_t       cwait;
+  u_int32_t       rwait;
+  u_int32_t       runtime;      /* time to keep simulating real users        */
+  char           *req;
+  int             req_size;     /* request size                              */
+
+  /* configuration data set by application based on user options */
+  int             finalize;     /* for request inmediate program termination */
+} DOS_CONFIG;
+
+
+/*****************************************************************************
+ * LOGFILE
+ *
+ *   This macro returns a pointer to 'stderr' if configuration logfile
+ *   parameter is not set.
+ */
+
+#define LOGFILE     (cfg->logfile ? cfg->logfile : stderr)
+
+/* program flags and configuration parameters */
+typedef struct _tag_DOS_PARAMETER {
+  char *name;
+  char *defvalue;
+  int  cfg_offset;
+  /* par_handler
+   *
+   *   This function handles this parameter. Its behaviour depends on
+   *   parameters pased:
+   *
+   *     READ MODE
+   *       Enabled when INT is zero or a positive value.
+   *       Passed pointer should point to a buffer with space for INT bytes.
+   *       If buffer size is 0 no data will be written to pointer, so it does
+   *       not matter if pointer to buffer is NULL. The function will return
+   *       the needed buffer size to copy data contained in this parameter.
+   *
+   *     WRITE MODE
+   *       Enabled when INT is a negative value.
+   *       Passed pointer to data must be NOT null, a pointer to data to copy
+   *       into parameter.
+   *
+   *     FREE MODE
+   *       Enabled when INT is a negative value and passed pointer to data is
+   *       NULL.
+   */
+  int (*par_handler)(struct _tag_DOS_PARAMETER *p, char *b, int s);
+} DOS_PARAMETER;
+
+extern DOS_PARAMETER dos_param_list[];
+
+typedef struct _tag_CMD_OPTION {
+  char shortopt;
+  char *longopt;
+  int  has_arg; /* 0 no arg; 1 yes; 2 optional */
+  char *param;
+  char *set_val;
+  int  (*handler)(char *);
+} DOS_CMD_OPTION;
+
+extern DOS_CMD_OPTION cmd_options[];
+
+int dos_config_init(int argc, char **argv, int *error);
+void dos_param_set(char *param, char *value);
+int dos_param_get(char *param, void *b);
+int dos_param_get_bool(char *param);
+int dos_param_get_int(char *param);
+int dos_param_get_str(char *param);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
